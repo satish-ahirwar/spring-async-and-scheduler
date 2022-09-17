02829709a803e22c574e18f9c2141adc4242f827
satish <satishlariya@gmail.com>
2022-09-17 14:06:17 +0530

    initial commit

---
+++ src/main/java/com/tech/SpringAsyncAndSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncAndSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAsyncAndSchedulerApplication.class, args);
+	}
+
+}
